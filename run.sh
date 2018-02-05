e27d41c7ec18834c927b00155a51af788bdaf4e7
vuvnn <36155439+vuvnn@users.noreply.github.com>
2018-02-05 20:39:07 +0700

    auto
    update

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get update
+apt-get upgrade
+apt-get install -y automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev make g++ git libgmp-dev 
+git clone https://github.com/magi-project/m-cpuminer-v2
+cd m-cpuminer-v2
+./autogen.sh
+CFLAG="-O2 mfpu=neon-vfpv4" ./configure
+make
